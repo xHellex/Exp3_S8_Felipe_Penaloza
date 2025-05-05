a27ac12da9edfcb9723a38947424bd19195df94e
Felipe Peñaloza <felipe.p_94@hotmail.com>
2025-05-04 23:35:22 -0400

    Felo
    tarea terminada

---
+++ src/main/java/com/mycompany/exp3_s8_felipe_penaloza/$1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exp3_s8_felipe_penaloza;
+
+/**
+ *
+ * @author Felip
+ */
+class $1 {
+    
+}
